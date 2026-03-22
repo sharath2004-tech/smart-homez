50c52f1e5d1a3c3970fd2d156e6200d7ed99edc0
sharath <2004sharath@gmail.com>
2026-03-22 10:09:44 +0530

    tt

--- src/pages/admin/AdminBookings.tsx
+++ src/pages/admin/AdminBookings.tsx
@@ -770,8 +770,6 @@ const AdminBookings = () => {
         </div>
       )}
 
-      )}
-
       {/* Workforce & Wages Modal */}
       {workforceBooking && (
         <div className="fixed inset-0 bg-black/60 z-50 flex items-center justify-center p-4 overflow-y-auto">
